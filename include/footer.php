ac24e840e27b07f4edf70b66ee1231c3d885ef86
nhanjunior14 <lephuocnhan0714@gmail.com>
2024-04-29 17:38:42 +0700

    footer

---
+++ include/footer.php
@@ -0,0 +1,40 @@
+<div class="text-danger" style="border: 2px solid"></div>
+<footer class="footer">
+    <div class="container">
+        <div class="bottom-footer">
+            <div class="row">
+                <div class="col-xs-12 col-sm-5 additional-info color-gray">
+                    <h5>About Food Delivery</h5>
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis, dolore. Doloremque quae corrupti quos porro laborum magni perspiciatis blanditiis ullam sed commodi officiis deserunt quidem, dignissimos natus possimus repellat eius.</p>
+                </div>
+                <div class="col-xs-12 col-sm-4 address color-gray">
+                    <h5>Address</h5>
+                    <p>Tan Hung Ward, District 7, Ho Chi Minh City.</p>
+                    <h5>Phone: <span class="text-danger">(+84)</span> 123-456-789</a></h5>
+                </div>
+                <div class="col-xs-12 col-sm-3 payment-options color-gray">
+                    <h5>Payment Options</h5>
+                    <ul>
+                        <li>
+                            <a href="#"><img src="./images/home_page/payment/paypal.png" alt="Paypal"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="./images/home_page/payment/bitcoin.png" alt="Bitcoin"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="./images/home_page/payment/mastercard.png" alt="Mastercard"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="./images/home_page/payment/stripe.png" alt="Stripe"></a>
+                        </li>
+                        <li>
+                            <a href="#"><img src="./images/home_page/payment/maestro.png" alt="Maestro"></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <hr>
+    </div>
+    <h5 style="text-align: center; margin-top: 16px">Coding by <a href="https://github.com/nhanjunior14" class="text-danger">Nhan Junior</a> © 2024 - Food Delivery</h5>
+</footer>
